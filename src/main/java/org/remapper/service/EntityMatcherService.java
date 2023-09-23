d99fd2be4d46e2847fa763f0dcaccbf3db2546ab
lyoubo <295967709@qq.com>
2023-09-23 10:44:11 +0800

    Update comments

--- src/main/java/org/remapper/service/EntityMatcherService.java
+++ src/main/java/org/remapper/service/EntityMatcherService.java
@@ -8,7 +8,7 @@ import org.remapper.handler.MatchingHandler;
 public interface EntityMatcherService {
 
     /**
-     * Detect refactorings performed in the specified commit.
+     * Match software entities in the specified commit.
      *
      * @param repository A git repository (from JGit library).
      * @param commitId   The SHA key that identifies the commit.
@@ -17,7 +17,7 @@ public interface EntityMatcherService {
     void matchAtCommit(Repository repository, String commitId, MatchingHandler handler);
 
     /**
-     * Detect refactorings performed in the specified commit.
+     * Match software entities in the specified commit.
      *
      * @param repository A git repository (from JGit library).
      * @param commitId   The SHA key that identifies the commit.
